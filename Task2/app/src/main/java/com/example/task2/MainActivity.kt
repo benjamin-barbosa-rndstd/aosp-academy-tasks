bc54788b6e5a72f1935f157cdc201975439d4abc
Benjamin Barbosa <benjamin.barbosa@randstad.com.mx>
2024-05-16 02:07:45 -0600

    Test: Add layout for button, this will launch another activity to simulate onPause state

--- Task2/app/src/main/java/com/example/task2/MainActivity.kt
+++ Task2/app/src/main/java/com/example/task2/MainActivity.kt
@@ -10,13 +10,23 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.coroutineScope
 import com.example.task2.ui.theme.Task2Theme
 import kotlinx.coroutines.launch
+
+/* Extra imports */
 import android.widget.TextView
 import android.os.Handler
+import android.widget.Button
 
 
 class MainActivity : ComponentActivity() {
+    /* Time variables */
     private var startTimeMillis: Long = 0
+    private var startBackGroundTime: Long = 0
+    private var startForeGroundTime: Long = 0
+
+    /* To manage layouts */
     private lateinit var foregroundTimeTextView: TextView
+
+
     private val handler = Handler()
 
     private val updateTimeTask = object : Runnable {
@@ -31,28 +41,60 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    /* In Background */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val stateView: TextView = findViewById(R.id.stateView)
+        stateView.text = "onCreate"
+
+
         foregroundTimeTextView = findViewById(R.id.foregroundTime)
+
+        /* For state button */
+        val stateButton: Button = findViewById(R.id.stateButton)
+        stateButton.setOnClickListener {
+            stateView.text = "Button clicked"
+            finish()
+        }
     }
+
+    /* In Background */
     override fun onStart() {
         super.onStart()
+        val stateView: TextView = findViewById(R.id.stateView)
+        stateView.text = "onStart"
+    }
+
+    /* In Foreground */
+    override fun onResume() {
+        super.onResume()
+        val stateView: TextView = findViewById(R.id.stateView)
+        stateView.text = "onResume"
+
         startTimeMillis = System.currentTimeMillis()
         handler.post(updateTimeTask)
     }
-    override fun onResume() {
-        super.onResume()
-        handler.post(updateTimeTask)
-    }
 
+    /* In Background */
     override fun onPause() {
         super.onPause()
+        val stateView: TextView = findViewById(R.id.stateView)
+        stateView.text = "onPause"
     }
 
+    /* In Background? */
     override fun onStop() {
         super.onStop()
+        val stateView: TextView = findViewById(R.id.stateView)
+        stateView.text = "onPause"
+
         handler.removeCallbacks(updateTimeTask)
+    }
+
+    /*  */
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 
